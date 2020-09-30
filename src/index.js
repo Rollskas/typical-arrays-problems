244cd352eb857b5e9152bf61cdf0a3980b03a19e
Rollskas <Rollskasanna@gmail.com>
2020-09-30 20:21:19 +0300

    feat: add all function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array)
+    if (array.length)
+      return array.sort(function(a,b){return a - b})[0];
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array)
+    if (array.length)
+      return Math.max.apply(null,array)
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array)
+    if (array.length)
+      return array.reduce((a,b)=> a + b) / array.length
   return 0;
 }
